b5137da90c0857382afd31a1e7c048023009888b
Sebas Notebook <sebassotelo97@gmail.com>
2025-08-05 05:53:37 -0300

    cas

--- src/componentes/layout/Layout.jsx
+++ src/componentes/layout/Layout.jsx
@@ -7,8 +7,8 @@ function Layout({ children }) {
   return (
     <div style={{ display: "grid" }}>
       <Head>
-        <title>Mecanico APP</title>
-        <meta name="description" content="APP web de MECANICO." />
+        <title>Asistente IA Sebas Sotelo</title>
+        <meta name="description" content="Asistente IA Sebas Sotelo." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
